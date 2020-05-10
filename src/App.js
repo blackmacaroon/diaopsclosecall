03c595feb271e274ee0e44a40ba4dda3fa9d39a1
Kayla <6lackmacaroon@gmail.com>
2020-05-10 14:59:07 -0600

    wired router, file scaffold for 3 main pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>hi hi hi hihi hi </p>
-      </header>
-      <section>
-      </section>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+         <p>hi hi hi hihi hi </p>
+        </header>
+        <section>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Login</Link>
+            </li>
+            <li>
+              <Link to="/request">Request Closure</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route path="/admin">
+            </Route>
+            <Route path="/request">
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </Router>
   );
 }
 
