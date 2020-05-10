216b95a3bfe3ea805860e6abccae428f03c456a8
Kayla <6lackmacaroon@gmail.com>
2020-05-10 15:05:54 -0600

    skeleton pages

--- src/components/home.js
+++ src/components/home.js
@@ -1,1 +1,10 @@
 import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <p>home page</p>
+        </div>
+    )
+}
+export default Home;
---
+++ src/components/request.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Request = () => {
+    return (
+        <div>
+            <p>request page</p>
+        </div>
+    )
+}
+export default Request;
