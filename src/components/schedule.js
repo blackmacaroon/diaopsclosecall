70d3cc7cb98644755beebfe578b0db45bcc16922
Kayla <6lackmacaroon@gmail.com>
2020-05-11 14:40:04 -0600

    idea spitball for schedule page

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -3,7 +3,10 @@ import React from "react";
 const Schedule = () => {
     return (
         <div className="Schedule">
-             <p>hi</p>
+             <h2>next 24 hours</h2>
+             <div><p>think weather app, this is today's forecast</p></div>
+             <div><p>think weather app, this is the 5 day forecast</p></div>
+             <div><p>month by month, including button for the past</p></div>
              <table>
                  <tr>closure calendar/graph/grid/table</tr>
                  <tr>closure details</tr>
