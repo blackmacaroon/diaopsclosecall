c523a272b082f261c809a0eeb8b626385d010d07
Kayla <6lackmacaroon@gmail.com>
2020-05-14 14:19:54 -0600

    install lots of packages for time and date

--- src/components/request.js
+++ src/components/request.js
@@ -1,17 +1,31 @@
+import 'date-fns';
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+import DateFnsUtils from '@date-io/date-fns';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from "@material-ui/core/Checkbox";
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import { IconButton, InputAdornment } from "@material-ui/core";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
 
 
-const Request = () => {
+export default function Request(){
+    const [selectedDate, setSelectedDate] = React.useState(new Date().toString());
+    const handleDateChange = (date) => {
+        setSelectedDate(date)
+    };
     return (
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <div className="Request">
             <header>
                 <h2>create a closure request:</h2>
@@ -53,19 +67,46 @@ const Request = () => {
                             </Select>
                             <FormHelperText>and</FormHelperText>
                         </FormControl>
-                        
+                        <KeyboardDatePicker
+                            format="MM/dd/yyyy"
+                            margin="normal"
+                            id="date-picker-inline"
+                            label="date"
+                            value={selectedDate}
+                            onChange={handleDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        />
+                        <KeyboardTimePicker
+                            margin="normal"
+                            id="time-picker"
+                            label="time"
+                            value={selectedDate}
+                            onChange={handleDateChange}
+                            InputProps={{
+                                endAdornment: (
+                                  <InputAdornment position="end">
+                                    <IconButton>
+                                      <WatchLaterIcon />
+                                    </IconButton>
+                                  </InputAdornment>
+                                ),
+                              }}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change time',
+                            }}
+                        />
                         
                     </Card>
-                            <p>moment input of closure time</p>
-                            <p>moment input of closure date</p>
-                            <p>text input of closure purpose</p>
-                            <p>text input of requestee email</p>
-                            <p>verification of requestee email</p>
-                    
+                        <p>text input of closure purpose</p>
+                        <p>text input of requestee email</p>
+                        <p>verification of requestee email</p>
+                
                 </Grid>
             </Grid>
             </form>
         </div>
+        </MuiPickersUtilsProvider>
     )
 }
-export default Request;
