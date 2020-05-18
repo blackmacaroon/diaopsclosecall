54fdd001f9d7dfe8cf2d1f0cb251498e483fd0c0
Kayla <6lackmacaroon@gmail.com>
2020-05-18 09:42:53 -0600

    hard coding image pixels to try imageMap

--- src/components/map.js
+++ src/components/map.js
@@ -7,7 +7,7 @@ function details(){
 export default function Map(){
     return(
         <map className="denMap" name="denMap">
-            <area shape="poly" coords="33, 39, 44, 55, 66, 77, 100, 140" alt="runway" onClick={details(this)}/>
+            <area shape="poly" coords="33, 100, 12, 140" alt="runway" onClick={details(this)}/>
         </map>
     )
 }
